15fd702b50b9506bdf675072ad61e84e008682c3
malinauskas.andrius@yahoo.com <malinauskas.andrius@yahoo.com>
2020-09-21 11:11:23 +0300

    Prideta suma

---
+++ TestinisProjektas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestinisProjektas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 5;
+            Suma(a, b);
+
+        }
+
+        static void Suma(int a, int b)
+        {
+            Console.WriteLine($"Suma = {a + b}");
+        }
+
+    }
+}
